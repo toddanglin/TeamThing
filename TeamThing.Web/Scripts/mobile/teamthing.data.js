2dc6e0250eda339b8905d62b924d661821fc57ea
Todd Anglin <anglin@telerik.com>
2012-06-21 02:50:44 -0500

    MANY additions to teamthing.data, primarily supporting new API endpoint calls and new X-Auth headers

--- TeamThing.Web/Scripts/mobile/teamthing.data.js
+++ TeamThing.Web/Scripts/mobile/teamthing.data.js
@@ -1,5 +1,6 @@
 var TeamThingData = function () {
     var _uThingsCache = new localStore("uThingsCache"),
+        _uTeamThingsCache = new localStore("uTeamThingsCache"),
 		_uTeamsCache = null,
 		_lastPollTime = null
     _private = {},
@@ -15,19 +16,22 @@ var TeamThingData = function () {
 		    userLogin: { path: "user/signin", verb: "POST", token: false },
 		    userOauth: { path: "user/oauth", verb: "POST", token: true },
 		    registerUser: { path: "user/register", verb: "POST", token: false },
-		    getUserThings: { path: "user/{key}/things?teamId={teamId}", verb: "GET", token: false },
+		    getUserThings: { path: "user/{key}/things?$filter=TeamId eq {teamId}", verb: "GET", token: false },
 		    getThing: { path: "thing/{key}", verb: "GET", token: false },
+            getTeamThings: { path: "team/{key}/things", verb: "GET", token: false },
+            getStarredThings: { path: "user/{key}/starred", verb:"GET", token:false},
+            getTeamMembers: { path: "team/{key}/members", verb: "GET", token: false },
 		    createThing: { path: "thing", verb: "POST", token: false },
 		    updateThingStatus: { path: "thing/{key}/updatestatus", verb: "PUT", token: false },
+            updateThingStar: {path: "thing/{key}/star", verb: "PUT", token: false},
+            updateThingUnStar: {path: "thing/{key}/unstar", verb: "PUT", token: false},
 		    deleteThing: { path: "thing/{key}", verb: "DELETE", token: false }
 		},
 		_paths = _onlinePaths,
 		_authInfo = new localStore("authInfo");
 
     _private = {
-        load: function (route, options) {
-            console.log("GETTING");
-
+        load: function (route, options) {            
             var path = route.path,
             	verb = route.verb,
             	requiresToken = route.token,
@@ -35,19 +39,17 @@ var TeamThingData = function () {
             	info = _authInfo.get(),
                 dfd = new $.Deferred();
 
-            //Append token to options if required
-            if (requiresToken) {
-                //If token is missing, abort processing
-                if (info.authToken == null) {
-                    var msg = "Unable to process request due to missing client OAuth token";
-                    console.log("AUTH ERROR", msg, info.authToken);
-                    dfd.reject(msg);
-                }
 
-                options = options || {};
-                options.accessToken = info.authToken;
-                options.provider = info.provider;
+            if(info.authToken != null){
+                $.ajaxSetup({
+                    'beforeSend': function(xhr){
+                        xhr.setRequestHeader("X-AuthProvider", info.provider);
+                        xhr.setRequestHeader("X-AuthToken", info.authToken);
+                    }
+                })
             }
+
+            console.log("GETTING", url, verb, options);
 
             //TODO: Use cached data before re-querying (if fresh)
             if (!dfd.isRejected()) {
@@ -93,7 +95,43 @@ var TeamThingData = function () {
             return dfd.promise();
         },
         getAllTeamThings: function (tid) {
+            var dfd = new $.Deferred(),
+                route = $.extend({}, _paths.getTeamThings);
 
+            route.path = route.path.replace(/{key}/g, tid);
+
+            _private.load(route, {})
+                .done(function (data) {
+                    var tmpDate = new Date(),
+                        _expires = tmpDate.setMinutes(tmpDate.getMinutes() + 10);
+
+                    _uTeamThingsCache.set({ data: data, expires: _expires });
+
+                    dfd.resolve(data);
+                })
+                .fail(function (err) {
+                    console.log("GET TEAM THINGS ERROR", err);
+                    dfd.resolve(null);
+                });
+
+            return dfd.promise();
+        },
+        getStarredTeamThings: function (tid, userId) {
+            var dfd = new $.Deferred(),
+                route = $.extend({}, _paths.getStarredThings);
+
+            route.path = route.path.replace(/{key}/g, userId);
+
+            _private.load(route, {teamId: tid})
+                .done(function (data) {
+                    dfd.resolve(data);
+                })
+                .fail(function (err) {
+                    console.log("GET STARRED THINGS ERROR", err);
+                    dfd.resolve(null);
+                });
+
+            return dfd.promise();
         },
         getUserTeams: function (uid) {
 
@@ -115,6 +153,26 @@ var TeamThingData = function () {
 
             return dfd.promise();
         },
+        getTeamMembers: function(tid){
+            var dfd = new $.Deferred(),
+                route = $.extend({}, _paths.getTeamMembers);
+
+            //TODO: Check for cache before loading
+
+            route.path = route.path.replace(/{key}/g, tid);
+
+            _private.load(route, {})
+                .done(function (data) {
+                    //TODO: Cache
+                    console.log("TEAM MEMBERS", data);
+                    dfd.resolve(data);
+                }).fail(function (err) {
+                    console.log("GET TEAM MEMBERS ERROR", err);
+                    dfd.resolve(null);
+                });
+
+            return dfd.promise();
+        },
         joinTeam: function (teamId, userId) {
             var dfd = new $.Deferred(),
 				route = $.extend({}, _paths.joinTeam);
@@ -130,6 +188,23 @@ var TeamThingData = function () {
 				    console.log("JOIN TEAM ERROR", err);
 				    dfd.resolve(null);
 				});
+
+            return dfd.promise();
+        },
+        createTeam: function(teamName, isPrivate, userId){
+            var dfd = new $.Deferred(),
+                route = $.extend({}, _paths.createTeam);
+
+
+            _private.load(route, { createdById: userId, name: teamName, isPublic: !isPrivate })
+                .done(function (data) {
+                    console.log("CREATE TEAM", data);
+                    dfd.resolve(data);
+                })
+                .fail(function (err) {
+                    console.log("CREATE TEAM ERROR", err);
+                    dfd.reject(err);
+                });
 
             return dfd.promise();
         },
@@ -192,6 +267,38 @@ var TeamThingData = function () {
 				.fail(function (err) {
 				    dfd.resolve(null);
 				});
+
+            return dfd.promise();
+        },
+        updateThingStar: function(thingId, userId){
+            var dfd = new $.Deferred(),
+                route = $.extend({}, _paths.updateThingStar);
+
+            route.path = route.path.replace(/{key}/g, thingId);
+
+            _private.load(route, {userId: userId})
+                .done(function (data) {
+                    dfd.resolve(data);
+                })
+                .fail(function (err) {
+                    dfd.resolve(null);
+                });
+
+            return dfd.promise();
+        },
+        updateThingUnStar: function(thingId, userId){
+            var dfd = new $.Deferred(),
+                route = $.extend({}, _paths.updateThingUnStar);
+
+            route.path = route.path.replace(/{key}/g, thingId);
+
+            _private.load(route, {userId: userId})
+                .done(function (data) {
+                    dfd.resolve(data);
+                })
+                .fail(function (err) {
+                    dfd.resolve(null);
+                });
 
             return dfd.promise();
         },
